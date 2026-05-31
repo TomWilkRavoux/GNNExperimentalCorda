d75f6608b1c9ae7ec174e625d30513dffc28f705
TomWilkRavoux <tomwilkwilk@gmail.com>
2026-05-31 13:11:56 +0200

    Essaie Inspecteur

---
+++ notebook/testExplainer.py
@@ -0,0 +1,21 @@
+import marimo
+
+__generated_with = "0.23.8"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    from torch_geometric.explain import Explainer, GNNExplainer
+
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
